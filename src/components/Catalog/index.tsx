01bbe5410cce44465def13186563304371143232
Brunno Sena <brunnow@hotmail.com>
2020-08-31 10:20:51 -0300

    Finally classes

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect} from 'react';
+
 import { IProduct } from '../../store/modules/cart/types';
-import api from '../../services/apit';
-import { addProductToCart } from '../../store/modules/cart/actions';
+import api from '../../services/api';
+import CatalogItem from '../CatalogItem';
 
 
-const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
+const Catalog: React.FC = () => {  
   const [catalog, setCatalog] = useState<IProduct[]>([])  
 
   useEffect(() => {
@@ -14,27 +13,13 @@ const Catalog: React.FC = () => {
     .then(res => setCatalog(res.data))
   }, [])
 
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, [dispatch])
-
   return (
     <main>
       <h1>Catalog</h1>
-
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {' - '}
-          <span>{product.price}</span>  {'  '}
-
-          <button 
-            onClick={() => handleAddProductToCart(product)}
-            type="button">Comprar</button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
-
-
   );
 }
 
